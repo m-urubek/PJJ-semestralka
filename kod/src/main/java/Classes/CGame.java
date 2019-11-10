46cf9d078c149096480b16596a1d9e2372462835
xurubek <xurubek@mendelu.cz>
2019-11-10 17:32:45 +0100

    upraveny getetry a settery v modelu zacatek deklarovani tridy CGame

--- kod/src/main/java/Classes/CGame.java
+++ kod/src/main/java/Classes/CGame.java
@@ -27,4 +27,16 @@ public class CGame {
     public void EndTurn() {
 
     }
+
+    public TCurrentPlayerTurn getM_currentPlayerTurn() {
+        return m_currentPlayerTurn;
+    }
+
+    public TState getM_playerState() {
+        return m_playerState;
+    }
+
+    public void setM_playerState(TState m_playerState) {
+        this.m_playerState = m_playerState;
+    }
 }
