7335a2b3cbf72dfe5569d149aebf00b640149b18
xurubek <xurubek@mendelu.cz>
2019-11-10 17:53:17 +0100

    vygenerovane prazdne testovaci metody k testum

---
+++ kod/src/test/java/Classes/CGameTest.java
@@ -0,0 +1,16 @@
+package Classes;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CGameTest {
+
+    @Test
+    public void newGame() {
+    }
+
+    @Test
+    public void endTurn() {
+    }
+}
