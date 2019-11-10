d6cd1b36a88881ae5be179c7dcd349fd14dc8bed
xurubek <xurubek@mendelu.cz>
2019-11-10 17:50:21 +0100

    vytvoreni struktury tridy CField

--- kod/src/main/java/Classes/CField.java
+++ kod/src/main/java/Classes/CField.java
@@ -1,4 +1,31 @@
 package Classes;
 
+import DataTypes.TPoint;
+
 public class CField {
+    private int m_x;
+    private int m_y;
+    private CFigurine m_figurine;
+
+    public CField(TPoint coordinates) {
+    }
+
+    public CField(TPoint coordinates, CFigurine figurine) {
+    }
+
+    public int getM_x() {
+        return m_x;
+    }
+
+    public int getM_y() {
+        return m_y;
+    }
+
+    public CFigurine getM_figurine() {
+        return m_figurine;
+    }
+
+    public void setM_figurine(CFigurine m_figurine) {
+        this.m_figurine = m_figurine;
+    }
 }
