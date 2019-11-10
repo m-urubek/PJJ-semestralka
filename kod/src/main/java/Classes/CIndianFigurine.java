df95b7cfcf02ecdb71e1849301cdfcb4f0e0041e
David Lacina <56764962+yuccatheplant@users.noreply.github.com>
2019-11-10 17:33:53 +0100

    Added inheritance

--- kod/src/main/java/Classes/CIndianFigurine.java
+++ kod/src/main/java/Classes/CIndianFigurine.java
@@ -1,4 +1,4 @@
 package Classes;
 
-public class CIndianFigurine {
+public class CIndianFigurine extends CFigurine {
 }
--- kod/src/main/java/Classes/CSettlerFigurine.java
+++ kod/src/main/java/Classes/CSettlerFigurine.java
@@ -1,4 +1,4 @@
 package Classes;
 
-public class CSettlerFigurine {
+public class CSettlerFigurine extends CFigurine {
 }
