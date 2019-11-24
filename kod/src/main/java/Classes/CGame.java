b6a94362b8760587c1fc5d74cf0713da0520793b
xurubek <xurubek@mendelu.cz>
2019-11-24 22:08:20 +0100

    EndTurn()

--- kod/src/main/java/Classes/CGame.java
+++ kod/src/main/java/Classes/CGame.java
@@ -1,6 +1,7 @@
 package Classes;
 
 import DataTypes.TCurrentPlayerTurn;
+import DataTypes.TPoint;
 import DataTypes.TState;
 
 import java.util.List;
@@ -13,7 +14,7 @@ public class CGame {
     public static TState m_playerState;
 
     public static void NewGame() {
-        if (m_gameLayout != null){
+        if (m_gameLayout != null) {
             EndGame();
         }
         m_gameLayout = new CGameLayout();
@@ -25,5 +26,69 @@ public class CGame {
 
     public static void EndTurn() {
         //TODO
+        for (CSettlerFigurine currentFigurine : CGame.m_settlerFigurines) {
+            TPoint point = new TPoint(currentFigurine.m_field.getM_x() + 2, currentFigurine.m_field.getM_y());
+            CField field = CGame.m_gameLayout.GetAt(point);
+            if (currentFigurine.isLegalMove(field)) {
+                CGame.m_settlerFigurines.remove(currentFigurine);
+                m_gameLayout.DeleteFieldContent(currentFigurine.m_field);
+            }
+
+            point = new TPoint(currentFigurine.m_field.getM_x() - 2, currentFigurine.m_field.getM_y());
+            field = CGame.m_gameLayout.GetAt(point);
+            if (currentFigurine.isLegalMove(field)) {
+                CGame.m_settlerFigurines.remove(currentFigurine);
+                m_gameLayout.DeleteFieldContent(currentFigurine.m_field);
+            }
+
+            point = new TPoint(currentFigurine.m_field.getM_x(), currentFigurine.m_field.getM_y() + 2);
+            field = CGame.m_gameLayout.GetAt(point);
+            if (currentFigurine.isLegalMove(field)) {
+                CGame.m_settlerFigurines.remove(currentFigurine);
+                m_gameLayout.DeleteFieldContent(currentFigurine.m_field);
+            }
+
+            point = new TPoint(currentFigurine.m_field.getM_x(), currentFigurine.m_field.getM_y() - 2);
+            field = CGame.m_gameLayout.GetAt(point);
+            if (currentFigurine.isLegalMove(field)) {
+                CGame.m_settlerFigurines.remove(currentFigurine);
+                m_gameLayout.DeleteFieldContent(currentFigurine.m_field);
+            }
+
+            point = new TPoint(currentFigurine.m_field.getM_x() + 2, currentFigurine.m_field.getM_y() + 2);
+            field = CGame.m_gameLayout.GetAt(point);
+            if (currentFigurine.isLegalMove(field)) {
+                CGame.m_settlerFigurines.remove(currentFigurine);
+                m_gameLayout.DeleteFieldContent(currentFigurine.m_field);
+            }
+
+            point = new TPoint(currentFigurine.m_field.getM_x() + 2, currentFigurine.m_field.getM_y() - 2);
+            field = CGame.m_gameLayout.GetAt(point);
+            if (currentFigurine.isLegalMove(field)) {
+                CGame.m_settlerFigurines.remove(currentFigurine);
+                m_gameLayout.DeleteFieldContent(currentFigurine.m_field);
+            }
+
+            point = new TPoint(currentFigurine.m_field.getM_x() - 2, currentFigurine.m_field.getM_y() - 2);
+            field = CGame.m_gameLayout.GetAt(point);
+            if (currentFigurine.isLegalMove(field)) {
+                CGame.m_settlerFigurines.remove(currentFigurine);
+                m_gameLayout.DeleteFieldContent(currentFigurine.m_field);
+            }
+
+            point = new TPoint(currentFigurine.m_field.getM_x() - 2, currentFigurine.m_field.getM_y() + 2);
+            field = CGame.m_gameLayout.GetAt(point);
+            if (currentFigurine.isLegalMove(field)) {
+                CGame.m_settlerFigurines.remove(currentFigurine);
+                m_gameLayout.DeleteFieldContent(currentFigurine.m_field);
+            }
+
+            if (CGame.m_currentPlayerTurn == TCurrentPlayerTurn.Indian) {
+                CGame.m_currentPlayerTurn = TCurrentPlayerTurn.Settler;
+            } else {
+                CGame.m_currentPlayerTurn = TCurrentPlayerTurn.Indian;
+            }
+        }
+
     }
 }
