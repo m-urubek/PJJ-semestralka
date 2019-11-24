4375abd805e619482d6d1462adca31a27d174596
xurubek <xurubek@mendelu.cz>
2019-11-24 19:46:50 +0100

    indexToCoords

--- kod/src/main/java/Classes/CGeneralHelper.java
+++ kod/src/main/java/Classes/CGeneralHelper.java
@@ -2,24 +2,123 @@ package Classes;
 
 import DataTypes.TIntResult;
 import DataTypes.TPoint;
+import DataTypes.TPointResult;
 
 public class CGeneralHelper {
 
-    public static TPoint indexToCoords (int index) {
-        //DONE - TODO Review!
-        switch(index) {
+    public static TPointResult indexToCoords(int index) {
+        //DONE
+        TPointResult result = new TPointResult();
+        switch (index) {
             case 0:
-                // code block
+                result.value = new TPoint(0, 0);
                 break;
             case 1:
-                // code block
+                result.value = new TPoint(0, 1);
+                break;
+            case 2:
+                result.value = new TPoint(0, 2);
+                break;
+            case 3:
+                result.value = new TPoint(1, 0);
+                break;
+            case 4:
+                result.value = new TPoint(1, 1);
+                break;
+            case 5:
+                result.value = new TPoint(1, 2);
+                break;
+            case 6:
+                result.value = new TPoint(2, 0);
+                break;
+            case 7:
+                result.value = new TPoint(2, 1);
+                break;
+            case 8:
+                result.value = new TPoint(2, 2);
+                break;
+            case 9:
+                result.value = new TPoint(2, 3);
+                break;
+            case 10:
+                result.value = new TPoint(2, 4);
+                break;
+            case 11:
+                result.value = new TPoint(2, 5);
+                break;
+            case 12:
+                result.value = new TPoint(2, 6);
+                break;
+            case 13:
+                result.value = new TPoint(3, 0);
+                break;
+            case 14:
+                result.value = new TPoint(3, 1);
+                break;
+            case 15:
+                result.value = new TPoint(3, 2);
+                break;
+            case 16:
+                result.value = new TPoint(3, 3);
+                break;
+            case 17:
+                result.value = new TPoint(3, 4);
+                break;
+            case 18:
+                result.value = new TPoint(3, 5);
+                break;
+            case 19:
+                result.value = new TPoint(3, 6);
+                break;
+            case 20:
+                result.value = new TPoint(4, 0);
+                break;
+            case 21:
+                result.value = new TPoint(4, 1);
+                break;
+            case 22:
+                result.value = new TPoint(4, 2);
+                break;
+            case 23:
+                result.value = new TPoint(4, 3);
+                break;
+            case 24:
+                result.value = new TPoint(4, 4);
+                break;
+            case 25:
+                result.value = new TPoint(4, 5);
+                break;
+            case 26:
+                result.value = new TPoint(4, 6);
+                break;
+            case 27:
+                result.value = new TPoint(5, 0);
+                break;
+            case 28:
+                result.value = new TPoint(5, 1);
+                break;
+            case 29:
+                result.value = new TPoint(5, 2);
+                break;
+            case 30:
+                result.value = new TPoint(6, 0);
+                break;
+            case 31:
+                result.value = new TPoint(6, 1);
+                break;
+            case 32:
+                result.value = new TPoint(6, 2);
                 break;
             default:
-                // code block
+                result.success = false;
+                result.message = "Entered index is out of bound";
+                return result;
         }
+        result.success = true;
+        return result;
     }
 
-    public static TIntResult coordsToIndex (TPoint coords) {
+    public static TIntResult coordsToIndex(TPoint coords) {
         //DONE
 
         int value = -1;
@@ -105,14 +204,15 @@ public class CGeneralHelper {
             value = 32;
 
         TIntResult result = new TIntResult();
-        if (value == -1){
+        if (value == -1) {
             result.message = "Coordinates were not entered correctly";
             result.success = false;
             return result;
+        } else {
+            result.value = value;
+            result.success = true;
+            return result;
         }
-        result.value = value;
-        result.success = true;
-        return result;
     }
 
 }
---
+++ kod/src/main/java/DataTypes/TPointResult.java
@@ -0,0 +1,7 @@
+package DataTypes;
+
+public class TPointResult {
+    public TPoint value;
+    public boolean success;
+    public String message;
+}
