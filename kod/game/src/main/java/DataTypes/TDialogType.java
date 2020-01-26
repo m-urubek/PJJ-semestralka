2fbbdc58fb6d553dd367461281fbda6465db6095
m-urubek <mar.urubek@gmail.com>
2020-01-26 16:36:46 +0100

    changed error dialog to multifunctional dialog

---
+++ kod/game/src/main/java/DataTypes/TDialogType.java
@@ -0,0 +1,6 @@
+package DataTypes;
+
+public enum TDialogType {
+    Error,
+    PlayerWon,
+}
---
+++ kod/game/src/main/java/MessageDialog.java
@@ -0,0 +1,54 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+//addObject(new MessageDialog(TDialogType.Error, e.getMessage(), this.getWidth(), this.getHeight()), this.getWidth()/2, this.getHeight()/2);
+
+import greenfoot.*;
+import DataTypes.TDialogType;
+
+public class MessageDialog extends Actor {
+    GreenfootImage inner;
+    int _width;
+    int _height;
+
+    private String insertNewLines(String string) {
+        for (int i = 0; i < string.length(); i++){
+            if (i%90==0){
+                string = string.substring(0, i) + "\n" + string.substring(i, string.length());
+                if (string.length()-1 != i){
+                    i++;
+                }
+            }
+        }
+        return string;
+    }
+
+    public MessageDialog(TDialogType dialogType, String text, int width, int height) {
+        _width = width;
+        _height = height;
+        if (dialogType == TDialogType.Error) {
+            this.inner = new GreenfootImage(insertNewLines(text), 30, Color.RED, new Color(0, 0, 0, 180));
+        }
+        if (dialogType == TDialogType.PlayerWon) {
+            this.inner = new GreenfootImage(text, 50, Color.RED, new Color(0, 0, 0, 180));
+        }
+        // inner.scale(width, height);
+    }
+
+    public void addedToWorld(World world) {
+        GreenfootImage outer = new GreenfootImage(_width, _height);
+        int leftX = (_width - this.inner.getWidth()) / 2;
+        int topY = (_height - this.inner.getHeight()) / 2;
+        outer.drawImage(this.inner, leftX, topY);
+        this.setImage(outer);
+    }
+
+    public void act() {
+        if (Greenfoot.mouseClicked(this)) {
+            this.getWorld().removeObject(this);
+        }
+
+    }
+}
